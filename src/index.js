dd659e3f5ddcaf567b19f1d0fe77e25fe53eade9
Ricardo Gamboa <ricardogamboa@live.com>
2022-11-08 17:00:47 -0600

    initial config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+import { saludar } from './js/components'
+import './styles.css'
+import img from './assets/img/webpack-logo.png';
+
+saludar( 'Ricardo' );
+
+
---
+++ src/js/components.js
@@ -0,0 +1,20 @@
+
+
+import '../css/components.css'
+import img from '../assets/img/webpack-logo.png';
+
+export const saludar = ( nombre ) => {
+    console.log('Creando etiqueta h1');
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${ nombre }!!!`;
+
+    document.body.append( h1 );
+
+    const newImg = document.createElement('img');
+    newImg.src = img;
+
+    document.body.append(newImg);        
+}
+
+
